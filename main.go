e54707fa0de13458465582014279a32e5b0dafb5
Nils Gustav Stråbø <nst@equinor.com>
2022-08-12 13:11:05 +0200

    side effect import only

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"fmt"
-
-	secretproviderclient "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned"
+	_ "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned"
 )
 
-func main() {
-	var client secretproviderclient.Interface
-
-	fmt.Print(client)
-}
+func main() {}
