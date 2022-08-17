883eeb32cfde8a410f12fddde5f7d8061139541c
Nils Gustav Stråbø <nst@equinor.com>
2022-08-17 14:21:49 +0200

    dsfs

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import (
-	_ "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned"
-)
+// _ "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned"
 
 func main() {}
