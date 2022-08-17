9543a27bdc7ae40221b03798337e70782ff02669
Nils Gustav Stråbø <nst@equinor.com>
2022-08-17 14:25:35 +0200

    gfdg

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
-// _ "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned"
+import _ "sigs.k8s.io/secrets-store-csi-driver/pkg/client/clientset/versioned"
 
 func main() {}
